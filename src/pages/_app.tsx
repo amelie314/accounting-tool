205c803060bd8c5c14a0e4a56577678814e76cc3
amelie314 <qangela314@gmail.com>
2023-11-01 21:33:33 +0800

    set component to its right type in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,30 @@
-import '../app/globals.css';
-import { auth } from '../firebaseConfig';
-import React, { useEffect, useState, ReactNode } from 'react';
-import Layout from '../app/layout';
-import Navbar from '../components/Navbar';
+/** @format */
 
-function MyApp({ Component, pageProps }) {
+import React, { useEffect, useState, ReactNode, ReactElement } from "react";
+import { AppProps } from "next/app"; // 從 next/app 中導入 AppProps 類型
+import { auth } from "../firebaseConfig";
+import Layout from "../app/layout";
+import Navbar from "../components/Navbar";
+
+// 使用 AppProps 並擴展它來包括類型為 ReactNode 的 getLayout 屬性
+interface MyAppProps extends AppProps {
+  Component: AppProps["Component"] & {
+    getLayout?: (page: ReactElement) => ReactNode;
+  };
+}
+
+function MyApp({ Component, pageProps }: MyAppProps) {
   const [currentUser, setCurrentUser] = useState(null);
 
   useEffect(() => {
-    return auth.onAuthStateChanged(user => {
+    return auth.onAuthStateChanged((user) => {
       setCurrentUser(user ? user : null);
     });
   }, []);
 
-  const getLayout = Component.getLayout || ((page) => <Layout>{page}</Layout>);
+  // 確保 getLayout 能接收 ReactElement 並返回 ReactNode
+  const getLayout =
+    Component.getLayout || ((page: ReactElement) => <Layout>{page}</Layout>);
 
   return getLayout(
     <>
