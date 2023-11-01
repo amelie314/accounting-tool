48ff7700a4e850280d8460a2492d382ea049f728
amelie314 <qangela314@gmail.com>
2023-11-01 21:22:46 +0800

    - revise Navbar.tsx

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -2,19 +2,21 @@
 
 type ListProps = {
   records: {
-    id: string; // Firestore 文檔 ID
+    id: string;
     amount: number;
     detail: string;
   }[];
   onDelete: (id: string) => void; // 使用字符串 ID 作為參數
 };
 
-
 function List({ records, onDelete }: ListProps) {
   return (
     <ul>
       {records.map((record) => (
-        <li key={record.id} className="text-purple-400 flex justify-between mb-1">
+        <li
+          key={record.id}
+          className="text-purple-400 flex justify-between mb-1"
+        >
           💲 {record.amount} 📒 {record.detail}
           <button
             className="text-pink-400 p-1 border border-pink-400 rounded hover:bg-pink-100"
